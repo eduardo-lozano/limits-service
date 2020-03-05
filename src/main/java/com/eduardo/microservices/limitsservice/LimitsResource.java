31a40d4d1c8b933d8a730cd7cab49c0332450fb5
Ed <ed.lozano.developer@gmail.com>
2020-03-04 21:07:31 -0600

    Implementing Hystrix (fault tolerance)

--- src/main/java/com/eduardo/microservices/limitsservice/LimitsResource.java
+++ src/main/java/com/eduardo/microservices/limitsservice/LimitsResource.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eduardo.microservices.limitsservice.bean.Limits;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class LimitsResource {
@@ -22,5 +23,17 @@ public class LimitsResource {
 		logger.info("{}", limits);
 		
 		return limits;
+	}
+	
+	@GetMapping("/faulty-limits-example")
+	@HystrixCommand(fallbackMethod="fallbackFaultyRetrieveConfiguration")   // Name of the fallback method (below)
+	public Limits faultyRetrieveConfiguration() {
+		throw new RuntimeException("Not available");   // Exception thrown here on purpose
+	}
+	
+	// Default method to be called when some exception is thrown, for any reason, in a service (in this case, above)
+	// This prevents the downstream services from failing miserably in case some upstream or external service(s) fail
+	public Limits fallbackFaultyRetrieveConfiguration() {
+		return new Limits(8, 888);
 	}
 }
